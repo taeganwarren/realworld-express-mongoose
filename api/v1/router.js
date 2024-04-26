b654804a3439582f46fecc3666f8c7aa6154e3b5
Taegan Warren <warretaega@gmail.com>
2024-04-26 15:29:15 -0700

    restructure

---
+++ api/v1/router.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import articles_router from './articles.js';
+import comments_router from './comments.js';
+import favorites_router from './favorites.js';
+import profiles_router from './profiles.js';
+import tags_router from './tags.js';
+import { users_router, user_router } from './users.js';
+
+const v1_router = Router();
+
+v1_router.use('/api/articles', articles_router);
+v1_router.use('/api/comments', comments_router);
+v1_router.use('/api/favorites', favorites_router);
+v1_router.use('/api/profiles', profiles_router);
+v1_router.use('/api/tags', tags_router);
+v1_router.use('/api/users', users_router);
+v1_router.use('/api/user', user_router);
+
+export default v1_router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import api_router from './api/router.js';
+import v1_router from './api/v1/router.js';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(api_router);
+app.use(v1_router);
 
 app.listen(PORT, async function () {
     console.log(`Example app listening on port: ${PORT}`);
